c792b4f924319a782f653745299b3bc22f51a711
Benno Bielmeier <github@bbenno.com>
2021-06-03 02:15:26 +0200

    Colorize status icons

--- functions/is-up.fish
+++ functions/is-up.fish
@@ -1,10 +1,17 @@
 function is-up
-	set up_status '✔ Up' '⚠ Down' '✖ Invalid'
+	function __is_up -S ; echo $url'	'(set_color green)✔(set_color normal) Up; end
+	function __is_down -S; echo $url'	'(set_color yellow)⚠(set_color normal) Down; end
+	function __is_invalid -S; echo $url'	'(set_color red)✖(set_color normal) Invalid; end
+
 	for url in $argv
-		set status_code 3
 		if string match -irq '^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]\.[a-z]{2,}$' $url
-			set status_code (curl -s 'https://isitup.org/'$url'.json' | jq '.status_code')
+			switch (curl -s 'https://isitup.org/'$url'.json' | jq '.status_code')
+				case 1; __is_up
+				case 2; __is_down
+				case 3; __is_invalid
+			end
+		else
+			__is_invalid
 		end
-		echo $url'	'$up_status[$status_code]
 	end
 end
